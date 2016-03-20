57e88cdd9a2840b5c04fda57c51047551fae2347
chentongtc@gmail.com <chentongtc@gmail.com>
2016-03-20 20:49:43 +0000

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+var ToDo = (function () {
+    function ToDo() {
+    }
+    return ToDo;
+}());
+var ToDoItem = (function () {
+    function ToDoItem() {
+    }
+    return ToDoItem;
+}());
+//# sourceMappingURL=main.js.map
---
+++ main.ts
@@ -0,0 +1,9 @@
+class ToDo {
+	private toDoItems: ToDoItem[];
+}
+
+class ToDoItem {
+	private item: String;
+	private createDate: Date;
+	private dueDate: Date;
+}
